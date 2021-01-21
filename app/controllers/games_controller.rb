5c2ca44c8d4dcaeddeb5d8e6313bd4f700f3be54
nobmurakami <mhpdm541@gmail.com>
2021-01-21 18:32:24 +0900

    ゲームの新規登録処理を実装

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,17 @@
 class GamesController < ApplicationController
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
+    @game.save
+    redirect_to new_game_path
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :description)
   end
 end
