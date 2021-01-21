fec1452ed0e14c7c870b2882799daac265a81592
nobmurakami <mhpdm541@gmail.com>
2021-01-22 00:02:28 +0900

    ページネーションの実装

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.all.order("created_at DESC")
+    @games = Game.page(params[:page]).per(10).order("created_at DESC")
   end
   
   def new
