ccf9a4b42b51b3c3dc2f41542455b3449840eb75
nobmurakami <mhpdm541@gmail.com>
2021-01-21 22:35:10 +0900

    メタスコアのバリデーション追加

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ApplicationRecord
   validates :title, presence: true
+  validates :metascore, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
