2cc652b2d3b72a85213c36c2f0ebca62520054e6
nobmurakami <mhpdm541@gmail.com>
2021-01-26 18:14:21 +0900

    #12 モデルとテーブル作成

---
+++ db/migrate/20210126091140_create_game_genres.rb
@@ -0,0 +1,9 @@
+class CreateGameGenres < ActiveRecord::Migration[6.0]
+  def change
+    create_table :game_genres do |t|
+      t.references :game, null: false, foreign_key: true
+      t.references :genre, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
