ebdbaca71e09c4049d5861670e2f7442c9a1c376
nobmurakami <mhpdm541@gmail.com>
2021-01-27 01:43:40 +0900

    rubocopで修正

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get games_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get games_create_url
     assert_response :success
   end
-
 end
