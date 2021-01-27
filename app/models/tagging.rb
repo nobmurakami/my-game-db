e045d2f8d68c3c752156026954b17e84013f184d
nobmurakami <mhpdm541@gmail.com>
2021-01-27 19:21:56 +0900

    #22 Userモデルを作成

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,4 +1,5 @@
 class Tagging < ApplicationRecord
   belongs_to :game
   belongs_to :tag
+  belongs_to :user
 end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :taggings, dependent: :destroy
+  has_many :tagging_games, through: :taggings, source: :game
+end
