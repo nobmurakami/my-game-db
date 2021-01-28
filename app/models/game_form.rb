52a2b6a93726101f88d592f3aa5d167e661ce5b1
nobmurakami <mhpdm541@gmail.com>
2021-01-29 03:28:30 +0900

    画像以外もSteamから取得できるようにした

--- app/models/game_form.rb
+++ app/models/game_form.rb
@@ -36,9 +36,22 @@ class GameForm
 
       @game.update!(title: title, description: description, metascore: metascore, release_date: release_date,
                     platform_id: platform.id, genres: genres, steam: steam)
-      @game.update!(steam_image: steam_image) if @game.steam.present?
       @game.update!(developers: developers, publishers: publishers)
       tags.map { |tag| Tagging.find_or_create_by!(game_id: @game.id, tag_id: tag.id, user_id: user_id )}
+
+      if @game.steam.present?
+        steam_data
+        @game.update!(steam_image: @steam_image)
+        @game.update!(description: @steam_description) if @game.description.blank?
+        @game.update!(metascore: @steam_metascore) if @game.metascore.blank?
+        @game.update!(release_date: @steam_release_date) if @game.release_date.blank?
+        @game.update!(genres: @steam_genres) if @game.genres.blank?
+        @game.update!(developers: @steam_developers) if @game.developers.blank?
+        @game.update!(publishers: @steam_publishers) if @game.publishers.blank?
+
+        tags = @steam_genres.map { |genre| Tag.find_or_create_by!(name: genre.name) }
+        tags.map { |tag| Tagging.find_or_create_by!(game_id: @game.id, tag_id: tag.id, user_id: user_id )}
+      end
     end
   end
 
@@ -59,9 +72,16 @@ class GameForm
     ActiveSupport::JSON.decode(response.read)
   end
 
-  def steam_image
-    if steam_json[steam_appids]["success"] == true
-      steam_json[steam_appids]["data"]["header_image"]
+  def steam_data
+    json = steam_json
+    if json[steam_appids]["success"] == true
+      @steam_image = json[steam_appids]["data"]["header_image"]
+      @steam_description = json[steam_appids]["data"]["short_description"]
+      @steam_metascore = json[steam_appids]["data"]["metacritic"]["score"]
+      @steam_release_date = Date.strptime(json[steam_appids]["data"]["release_date"]["date"], '%Y年%m月%d日')
+      @steam_genres = json[steam_appids]["data"]["genres"].map { |genre| Genre.find_or_create_by!(name: genre["description"]) }
+      @steam_developers = json[steam_appids]["data"]["developers"].map { |dev| Company.find_or_create_by!(name: dev) }
+      @steam_publishers = json[steam_appids]["data"]["publishers"].map { |pub| Company.find_or_create_by!(name: pub) }
     end
   end
 end
