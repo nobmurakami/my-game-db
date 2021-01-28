c99c27228e7f03fa0a52807e0ce927b63cb2a9c6
nobmurakami <mhpdm541@gmail.com>
2021-01-28 19:37:58 +0900

    #23 自分のタグ機能の実装

---
+++ app/controllers/taggings_controller.rb
@@ -0,0 +1,24 @@
+class TaggingsController < ApplicationController
+  def index
+  end
+
+  def create
+    if tag_params[:tag].present?
+      tag = Tag.find_or_create_by(name: tag_params[:tag]) 
+      Tagging.find_or_create_by(game_id: tag_params[:game_id], tag_id: tag.id, user_id: tag_params[:user_id])
+      redirect_to game_path(tag_params[:game_id])
+    end
+  end
+
+  def destroy
+    tagging = Tagging.find(params[:id])
+    tagging.destroy
+    redirect_to game_path(params[:game_id])
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:tag).merge(user_id: current_user.id, game_id: params[:game_id])
+  end
+end
