76389e89cd6f2e64c65d79383d475f38fa6a2b0c
nobmurakami <mhpdm541@gmail.com>
2021-01-29 18:21:42 +0900

    #34 PLAYINGボタンとPLAYEDボタンを実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,18 @@ class ListsController < ApplicationController
     redirect_to game_path(params[:game_id])
   end
 
+  def playing
+    list = current_user.playing_lists.find_or_initialize_by(game_id: params[:game_id])
+    list.save
+    redirect_to game_path(params[:game_id])
+  end
+
+  def played
+    list = current_user.played_lists.find_or_initialize_by(game_id: params[:game_id])
+    list.save
+    redirect_to game_path(params[:game_id])
+  end
+
   def destroy
     list = List.find_by(game_id: params[:game_id], user_id: current_user.id)
     list.destroy
