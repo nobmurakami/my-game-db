9564194197a9cd9ade6f27ed17becdfd607bd5dd
nobmurakami <mhpdm541@gmail.com>
2021-02-05 13:25:17 +0900

    ヘッダーに検索ボックスを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
+
+  def set_search
+    @search = Game.ransack(params[:header_search], search_key: :header_search)
+    @search_games = @search.result(distinct: true).page(params[:page]).per(10).order("created_at DESC")
+  end
 
   private
 
