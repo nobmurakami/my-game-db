438abe99fd26cae01eaa23a89f2b3448d3c07431
nobmurakami <mhpdm541@gmail.com>
2021-02-15 15:54:47 +0900

    #87 お気に入りボタン

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,13 @@
 class FavoritesController < ApplicationController
+  def create
+    favorite = current_user.favorites.build(game_id: params[:game_id])
+    favorite.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    favorite = Favorite.find_by(game_id: params[:game_id], user_id: current_user.id)
+    favorite.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
