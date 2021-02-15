a49a9a51fc389cdedc8e8e70cfc1f33f126bea44
nobmurakami <mhpdm541@gmail.com>
2021-02-15 22:05:16 +0900

    #91 お気に入りボタンをAjax化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,21 @@
 class FavoritesController < ApplicationController
+  before_action :game_params
+
   def create
     favorite = current_user.favorites.build(game_id: params[:game_id])
     favorite.save
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     favorite = Favorite.find_by(game_id: params[:game_id], user_id: current_user.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def game_params
+    @game = Game.find(params[:game_id])
   end
 end
