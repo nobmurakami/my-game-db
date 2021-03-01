ca040b87a71193f9defd5e9b06ffde85a2342810
nobmurakami <mhpdm541@gmail.com>
2021-03-01 23:31:07 +0900

    Rubocopによる修正

--- spec/support/list_support.rb
+++ spec/support/list_support.rb
@@ -2,7 +2,7 @@ module ListSupport
   def check_create_list(game, button)
     expect do
       click_on(button)
-      visit current_path #機能のAjax化に伴い、テストが通らなくなったため追記
+      visit current_path # 機能のAjax化に伴い、テストが通らなくなったため追記
     end.to change { List.count }.by(1)
     expect(current_path).to eq game_path(game)
   end
@@ -10,7 +10,7 @@ module ListSupport
   def check_move_list(game, button)
     expect do
       click_on(button)
-      visit current_path #機能のAjax化に伴い、テストが通らなくなったため追記
+      visit current_path # 機能のAjax化に伴い、テストが通らなくなったため追記
     end.to change { List.count }.by(0)
     expect(current_path).to eq game_path(game)
   end
@@ -18,7 +18,7 @@ module ListSupport
   def check_delete_list(game, button)
     expect do
       click_on(button)
-      visit current_path #機能のAjax化に伴い、テストが通らなくなったため追記
+      visit current_path # 機能のAjax化に伴い、テストが通らなくなったため追記
     end.to change { List.count }.by(-1)
     expect(current_path).to eq game_path(game)
   end
