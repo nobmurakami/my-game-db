5b0865cfc319dd41c0c61ea4a548c15b7e91feb6
nobmurakami <mhpdm541@gmail.com>
2021-03-01 19:53:15 +0900

    #138 プレイ状況ボタンのテストにリロードを追加

--- spec/support/list_support.rb
+++ spec/support/list_support.rb
@@ -2,6 +2,7 @@ module ListSupport
   def check_create_list(game, button)
     expect do
       click_on(button)
+      visit current_path #機能のAjax化に伴い、テストが通らなくなったため追記
     end.to change { List.count }.by(1)
     expect(current_path).to eq game_path(game)
   end
@@ -9,6 +10,7 @@ module ListSupport
   def check_move_list(game, button)
     expect do
       click_on(button)
+      visit current_path #機能のAjax化に伴い、テストが通らなくなったため追記
     end.to change { List.count }.by(0)
     expect(current_path).to eq game_path(game)
   end
@@ -16,6 +18,7 @@ module ListSupport
   def check_delete_list(game, button)
     expect do
       click_on(button)
+      visit current_path #機能のAjax化に伴い、テストが通らなくなったため追記
     end.to change { List.count }.by(-1)
     expect(current_path).to eq game_path(game)
   end
